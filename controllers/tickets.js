cb2800839ee9f1e636977de23c829b37b703676d
Alex Perkowski <perkowskialex@gmail.com>
2019-11-19 16:30:30 -0600

    submit ticket routes back to flight id

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -3,8 +3,16 @@ let Ticket = require('../models/ticket')
 
 module.exports = {
     new: newTicket,
+    create
 }
 
 function newTicket(req,res) {
     res.render('tickets/new', {title: 'Add Ticket', flightId: req.params.id});
+}
+
+function create(req,res) {
+    req.body.flight = req.params.id;  //flight property on the ticket
+    Ticket.create(req.body, function(err,ticket){
+        res.redirect(`/flights/${req.body.flight}`)
+    })
 }
--- routes/tickets.js
+++ routes/tickets.js
@@ -3,5 +3,6 @@ let router = express.Router();
 let ticketsCtrl = require('../controllers/tickets');
 
 router.get('/flights/:id/tickets/new', ticketsCtrl.new);
+router.post('/flights/:id/tickets', ticketsCtrl.create);
 
 module.exports = router;
